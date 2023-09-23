4198c8e8d1cd013af9bd33ecf1bedbbcdb5cfbf5
PredoLuskas <sntpedrolucas@gmail.com>
2023-09-23 19:35:11 -0300

    -Versão funcionando do SFJ com valores iniciais maior que 0 -Tempos medios adicionados

--- src/escalonadores/Escalonador.java
+++ src/escalonadores/Escalonador.java
@@ -62,98 +62,6 @@ public class Escalonador {
         System.out.println("FCFS " + String.format("Media tempo retorno: %.2f", mediaTempoRetorno) + " " + String.format("Media tempo resposta: %.1f", mediaTempoResposta) + " " + String.format("Media tempo Espera: %.1f", mediaTempoEspera));
     }
 
-/*    public void SJF() throws IOException {
-        List<Process> processos = new LinkedList<>();
-
-        processos = IOFiles.readerFile(path);  // Adicione processos à lista (nome, tempo de chegada, tempo de execução)
-
-        List<Process> lista = new LinkedList<>(processos);
-
-        assert processos != null;
-        lista.sort(Comparator.comparingInt(Process::getTempoDeExecucao)); //Ordenando por tempo de execução os processos que chegaram
-
-        int tempoAtual = 0;
-        int tempoInicio = 0;
-
-        // Calcular métricas
-        double tempoEspera = 0;
-        double tempoRetorno = 0;
-        double tempoResposta = 0;
-
-        double tempoEsperaTotal = 0;
-        double tempoRetornoTotal = 0;
-        double tempoRespostaTotal = 0;
-
-        Queue<Process> processoAux = new LinkedList<>() ;
-
-        Queue<Process> filaOrdenada = new LinkedList<>(lista);
-
-
-        for (Process processo : filaOrdenada) {
-
-                if (tempoAtual < processo.getTempoDeChegada()) {
-                    // Se o processo ainda não chegou, espere
-
-                    processoAux.add(processo);
-                   tempoAtual = processo.getTempoDeChegada();
-                   filaOrdenada.poll();
-                    lista.addAll(filaOrdenada);
-                    System.out.println(processo.getTempoDeChegada() + " " + processo.getTempoDeExecucao());
-
-                    continue;
-
-                }
-            System.out.println(processo.getTempoDeChegada() + " " + processo.getTempoDeExecucao());
-            processoAux.add(processo);
-        }
-
-
-/*
-                int tempoDeEntrada = Math.max(tempoAtual, processo.getTempoDeChegada());
-                tempoResposta = tempoDeEntrada - processo.getTempoDeChegada();
-                tempoRespostaTotal += tempoResposta; // Tempo Resposta
-                tempoEsperaTotal = tempoRespostaTotal; //Tempo de espera - No FCFS o tempo de  resposta é igual ao tempo de espera
-
-                tempoAtual = tempoDeEntrada + processo.getTempoDeExecucao();
-                tempoRetorno = (tempoAtual - processo.getTempoDeChegada());
-                tempoRetornoTotal += tempoRetorno; //Tempo Retorno
-            }
-        }
-
-
-        // Calcular valores médios necessários
-        double mediaTempoRetorno = tempoRetornoTotal / processos.size();
-        double mediaTempoResposta = tempoRespostaTotal / processos.size();
-        double mediaTempoEspera = tempoEsperaTotal / processos.size();
-
-
-        // Imprimir resultados
-        System.out.println("SJF " + String.format("Media tempo retorno: %.2f", mediaTempoRetorno) + " " + String.format("Media tempo resposta: %.1f", mediaTempoResposta) + " " + String.format("Media tempo Espera: %.1f", mediaTempoEspera));
-    }*/
-/*
-    public void SJFT() throws IOException {
-        Queue<Process> processos = new ArrayList<>();
-
-        processos = IOFiles.readerFile(path);  // Adicione processos à lista (nome, tempo de chegada, tempo de execução)
-
-        assert processos != null;
-        processos.sort(Comparator.comparingInt(Process::getTempoDeExecucao));
-
-        // Simular a execução dos processos
-        int tempoAtual = 0;
-        for (Process processo : processos) {
-            System.out.println(processo.getNomeProcesso() + " " + processo.getTempoDeChegada() + " " + processo.getTempoDeExecucao());
-            if (tempoAtual < processo.getTempoDeChegada()) {
-                // Se o processo ainda não chegou, espere
-                tempoAtual = processo.getTempoDeChegada();
-            }
-
-
-            System.out.println("Executando " + processo.getNomeProcesso() + " no tempo " + tempoAtual);
-            tempoAtual += processo.getTempoDeExecucao();
-        }
-    }*/
-
     public void roundRobin() throws IOException {
         int tempoAtual = 0;
         List<Process> listaDeProntidao = new ArrayList<>();
@@ -195,7 +103,7 @@ public class Escalonador {
 
             if (listaDeProntidao.get(0).getQuantProcess() == 0) {
                 tempoResposta = tempoAtual - listaDeProntidao.get(0).getTempoDeChegada();
-                tempoRespostaTotal+=tempoResposta;
+                tempoRespostaTotal += tempoResposta;
             }
 
             listaDeProntidao.get(0).setQuantProcess(++i);
@@ -226,7 +134,97 @@ public class Escalonador {
             tempoRetorno = (tempoAtual - processo.getTempoDeChegada());*/
         }
 
-        System.out.println("Media tempo retorno: " + tempoRetornoTotal / tamList + "media tempo resposta:" +tempoRespostaTotal / tamList );
+        System.out.println("Media tempo retorno: " + tempoRetornoTotal / tamList + "media tempo resposta:" + tempoRespostaTotal / tamList);
     }
+
+    public void SJF() throws IOException {
+        int tempoAtual = 0;
+        List<Process> listaDeProntidao = new ArrayList<>();
+
+        listaDeProntidao = IOFiles.readerFile(path);  // Adicione processos à lista (nome, tempo de chegada, tempo de execução)
+
+        listaDeProntidao.sort(Comparator.comparingInt(Process::getTempoDeExecucao)); //Ordenando por tempo de chegada os processos que chegaram
+
+
+        int quantum = 2; // Tamanho do quantum em unidades de tempo
+
+        double tempoEspera = 0;
+        double tempoRetorno = 0;
+        double tempoResposta = 0;
+        int tamList = listaDeProntidao.size();
+
+        double tempoEsperaTotal = 0;
+        double tempoRetornoTotal = 0;
+        double tempoRespostaTotal = 0;
+        double tempoInicial;
+        List<Process> listaAux = new ArrayList<>();
+
+        for (Process process : listaDeProntidao) {
+            process.setQuantProcess(0);
+        }
+        int i = 0;
+        int aux = 0;
+        while (!listaDeProntidao.isEmpty()) {
+            Process processoAtual = listaDeProntidao.get(0);
+
+            // Verifique se o processo chegou antes do tempo atual
+            if (processoAtual.getTempoDeChegada() > tempoAtual) {
+                for (Process process : listaDeProntidao) {
+                    if (process.getTempoDeChegada() <= tempoAtual) {
+                        aux = i;
+                        continue;
+                    }
+                    i++;
+                }
+                if(processoAtual.getTempoDeChegada() > tempoAtual && aux == 0){
+                    listaDeProntidao.sort(Comparator.comparingInt(Process::getTempoDeChegada));
+                    tempoAtual = listaDeProntidao.get(aux).getTempoDeChegada();
+                }
+            }
+
+            tempoInicial = tempoAtual;
+            tempoAtual += listaDeProntidao.get(aux).getTempoDeExecucao();
+            System.out.println(" terminou em " + tempoAtual + " unidades de tempo.");
+
+            double tempoDeEntrada = Math.max(tempoInicial, listaDeProntidao.get(aux).getTempoDeChegada());
+            tempoResposta = tempoDeEntrada - listaDeProntidao.get(aux).getTempoDeChegada();
+            tempoRespostaTotal += tempoResposta; // Tempo Resposta
+            tempoEsperaTotal = tempoRespostaTotal; //Tempo de espera - No FCFS o tempo de  resposta é igual ao tempo de espera
+
+            tempoRetorno = (tempoAtual - listaDeProntidao.get(aux).getTempoDeChegada());
+            tempoRetornoTotal += tempoRetorno; //Tempo Retorno
+
+            listaDeProntidao.remove(aux);
+            listaDeProntidao.sort(Comparator.comparingInt(Process::getTempoDeExecucao));
+            i = 0;
+            aux = 0;
+
+        }
+        double mediaTempoRetorno = tempoRetornoTotal / tamList;
+        double mediaTempoResposta = tempoRespostaTotal / tamList;
+        double mediaTempoEspera = tempoEsperaTotal / tamList;
+
+
+        // Imprimir resultados
+        System.out.println("FCFS " + String.format("Media tempo retorno: %.2f", mediaTempoRetorno) + " " + String.format("Media tempo resposta: %.2f", mediaTempoResposta) + " " + String.format("Media tempo Espera: %.2f", mediaTempoEspera));
+    }
+
+        /*
+            tempoAtual += listaDeProntidao.get(0).getTempoDeExecucao();
+
+            // Verifique se o processo ainda não terminou
+            if (processoAtual.getTempoDeExecucao()  0) {
+                // Coloque o processo no final da lista
+                if (processoAtual.getTempoDeExecucao() == 0) {
+                    // O processo terminou, imprima informações
+                    System.out.println(" terminou em " + tempoAtual + " unidades de tempo.");
+                    tempoRetorno = tempoAtual - listaDeProntidao.get(0).getTempoDeChegada();
+                    tempoRetornoTotal += tempoRetorno;
+                    listaDeProntidao.remove(0); // Remova da posição atual
+                    continue;
+                }
+                listaDeProntidao.add(processoAtual);
+                listaDeProntidao.remove(0); // Remova da posição atual*/
 }
+
 
